c5e5b21150dca3341e9e63c175cd2861de0ee6d0
Shaughn-Codes <shaughn.p.bulgar@gmail.com>
2024-07-02 16:15:15 -0400

    setting up spring boot project

---
+++ f1-dashboard/src/main/java/com/example/f1_dashboard/F1DashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.f1_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1DashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1DashboardApplication.class, args);
+	}
+
+}
