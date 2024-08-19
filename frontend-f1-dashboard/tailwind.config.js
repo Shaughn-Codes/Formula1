f1dde272005beedcaa38268251bc6147755408f9
Shaughn-Codes <shaughn.p.bulgar@gmail.com>
2024-08-19 11:55:34 -0400

    adding font, updaing nav to be reused, adding constrant to update current year instead of adding it manually

--- frontend-f1-dashboard/tailwind.config.js
+++ frontend-f1-dashboard/tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        racing: ['"Racing Sans One"', 'sans-serif']
+      }
+    },
   },
   plugins: [
     require('daisyui'),
